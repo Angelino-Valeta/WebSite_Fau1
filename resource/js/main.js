dce8b9f5477ad842db21b73fd646b698f0a38086
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-04 23:57:20 +0100

    get others elements

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,4 +1,9 @@
 const btnMeses = document.querySelector('.meses')
+const formRecolhaDados = document.querySelector('.av-getData')
+const table = document.querySelector('.av-table')
+const genero = document.getElementsByName('sexo')
+
+
 const meses = ['Fevereiro',' Março', ' Abril', ' Maio', ' Junho', ' Julho', ' Agosto', ' Setembro', ' Outubro', ' Novembro', 'Dezembro']
 
 btnMeses.addEventListener('click', () => {
