07ce8a81ac5b550ab738afbc5c2abb1c3f1c151b
Angelino Valeta <angelinovaleta@gmail.com>
2020-05-04 23:53:04 +0100

    clicke event for meses

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,2 +1,6 @@
 const btnMeses = document.querySelector('.meses')
 const meses = ['Fevereiro',' Março', ' Abril', ' Maio', ' Junho', ' Julho', ' Agosto', ' Setembro', ' Outubro', ' Novembro', 'Dezembro']
+
+btnMeses.addEventListener('click', () => {
+    alert(meses)
+})
